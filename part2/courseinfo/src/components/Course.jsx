1ce84155bdd279f62392ae65de3f677a981a06af
Tomi Nikku <tomi.jmn@gmail.com>
2023-09-30 15:44:14 +0300

    exc 2.2 done

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -17,45 +17,25 @@ const Course = ({ course }) => {
         )
     }
 
+    const Total = (props) => {
+        const content = props.content
+        const exercises = content.map((part) => part.exercises)
+        const totalExercises = exercises.reduce((total, exerciseAmount) => total + exerciseAmount, 0)
+        return(
+            <p><b>total of {totalExercises}</b></p>
+        )
+    }
+
+
     return(
     <div>
         <Header name={course.name} />
         <Content content={course.parts} />
+        <Total content={course.parts} />
     </div>
     )
 }
 
-
-
-    //   const Part = (props) => {
-    //     return (
-    //       <p>{props.name} {props.excs}</p>
-    //     )
-    //   }
-
-
-    //   const Content = (props) => {
-    //     const part1 = props.parts.parts[0]
-    //     const part2 = props.parts.parts[1]
-    //     const part3 = props.parts.parts[2]
-    //     return (
-    //       <div>
-    //         <Part name={part1.name} excs={part1.exercises}/>
-    //         <Part name={part2.name} excs={part2.exercises}/>
-    //         <Part name={part3.name} excs={part3.exercises}/>
-    //       </div>
-    //     )
-    //   }
-
-
-    //   const Total = (props) => {
-    //     const parts = props.exercises.parts
-    //     const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
-    //     return (
-    //       <p>Number of exercises {sum}</p>
-    //     ) 
-    //   }
-  
   
   
 export default Course
