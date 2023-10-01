91a40a518fac96717dce41ec75a78e79f0542f3d
Tomi Nikku <tomi.jmn@gmail.com>
2023-10-01 22:58:12 +0300

    exc 2.9 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const RenderPersons = (props) => {
+const RenderAllPersons = (props) => {
   const persons = props.content
   return(
     <div>
@@ -13,12 +13,36 @@ const RenderPersons = (props) => {
   )
 }
 
+const RenderFoundPersons = (props) => {
+  const searchTerm = props.searchTerm.toLowerCase()
+  const personObjects = props.content
+  const personsToRender = []
+  const personNames = props.content.map((person) => person.name.toLowerCase())
+  const finder = personNames.map((personName, index) => personName.includes(searchTerm) 
+  ? personsToRender.push(personObjects[index])
+  : null)
+  return(
+    <div>
+      {personsToRender.map((persons, i) =>
+      <p key={i}>
+        {persons.name} {persons.phonenumber}
+      </p>
+      )}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phonenumber: '040-1231244' }
-  ]) 
+    { name: 'Arto Hellas', phonenumber: '040-123456' },
+    { name: 'Ada Lovelace', phonenumber: '39-44-5323523' },
+    { name: 'Dan Abramov', phonenumber: '12-43-234345' },
+    { name: 'Mary Poppendieck', phonenumber: '39-23-6423122' }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [findPerson, setFindPerson] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -42,9 +66,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFindChange = (event) => {
+    setFindPerson(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>PHONEBOOK</h1>
+      <div>
+        filter shown with:
+        <input value={findPerson}
+        onChange={handleFindChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: 
@@ -60,8 +94,11 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <RenderPersons content={persons} />
+      <h2>numbers</h2>
+      {findPerson == '' 
+      ? <RenderAllPersons content={persons} />
+      : <RenderFoundPersons content={persons} searchTerm={findPerson} />
+        }
     </div>
   )
 
